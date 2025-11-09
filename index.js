58b7a23e8d90a555b48016a778683c64c501b613
shahriar429 <shahriarislam429@gmail.com>
2025-11-09 16:24:45 +0600

    add users api

--- index.js
+++ index.js
@@ -20,12 +20,70 @@ const client = new MongoClient(uri, {
   },
 });
 
+async function run() {
+  try {
+    // await client.connect();
+    const db = client.db("habitify_db");
+    const habitCollection = db.collection("habits");
+    const usersCollection = db.collection("users");
+    // USER APIs
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const email = req.body.email;
+      const query = { email: email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        res.send({ message: "user already exist, no need to insert" });
+      } else {
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      }
+    });
+
+    app.get("/users", async (req, res) => {
+      const email = req.query.email;
+      console.log("Fetching transactions for:", email);
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+      const cursor = usersCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Update a user info by ID
+    app.patch("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          name: updatedUser.name,
+          email: updatedUser.email,
+          image: updatedUser.image,
+        },
+      };
+      const result = await usersCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Habit-Tracker server is running");
+  res.send("Habitify server is running");
 });
-
-
-
 app.listen(port, () => {
-  console.log(`Habit-tracker server listening on port ${port}`);
+  console.log(`Habitify server listening on port ${port}`);
 });
