1384c9119d2c664ab04d1cb80edaeefe92c2a54b
shahriar429 <shahriarislam429@gmail.com>
2025-11-10 16:32:06 +0600

    delete habit api

--- index.js
+++ index.js
@@ -163,13 +163,13 @@ async function run() {
       res.send(result);
     });
 
-    // // delete-habit
-    // app.delete("/habits/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await habitCollection.deleteOne(query);
-    //   res.send(result);
-    // });
+    // delete-habit
+    app.delete("/habits/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await habitCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
